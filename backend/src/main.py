ed593819585133ab8f227306d46192176871ac82
Ryan McCauley <ryanmcly@gmail.com>
2025-02-23 01:28:54 -0500

    remove form-prompt-page

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers.challenges_controller import challenges_router
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 app.include_router(challenges_router)
 
